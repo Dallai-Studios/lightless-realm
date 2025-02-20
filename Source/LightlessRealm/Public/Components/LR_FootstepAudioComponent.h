05b5d6cfa9f19f037d40d980f6b01bf2c6661618
Renan Souza <renan.souza@powerpush.com.br>
2025-02-19 23:27:30 -0300

    new audio component

---
+++ Source/LightlessRealm/Public/Components/LR_FootstepAudioComponent.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "LR_FootstepAudioComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LIGHTLESSREALM_API ULR_FootstepAudioComponent : public UActorComponent {
+	GENERATED_BODY()
+
+public:
+	// ======================================================
+	// Ciclo de vida:
+	// ======================================================
+	ULR_FootstepAudioComponent();
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
